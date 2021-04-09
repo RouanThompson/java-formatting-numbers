7d1071dd6840c95a1a96c6dba2ebeed03062f6f0
rouanthompson <rouan.thompson@gmail.com>
2021-04-09 19:38:42 -0400

    Prints results for NumberFormat

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -1,8 +1,31 @@
 package com.FirstJavaSteps;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double num = 0.1;
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        // getCurrencyInstance() is a factory maker it returns a new object
+        // cannot use new to make a new instance of a abstract class NumberFormat.
+
+        String currencyResult = currency.format(num);
+        // currency now has access to format from the NumberFormat class which formats with $ and ,
+        // takes a number returns a string
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String percentResult = percent.format(num);
+        // getPercentInstance() method format. Converts number to percent and returns a string.
+
+        // Method Chaining -------------------
+        // involves using multiple methods after another
+
+        String chainingResult = NumberFormat.getCurrencyInstance().format(num);
+        // If a variable will only be used once, method chain instead.
+
+        System.out.println(percentResult);
+        System.out.println(currencyResult);
+        System.out.println(chainingResult);
     }
 }
